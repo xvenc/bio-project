401e2c7cf0fc12cfd49c45b16d0e3939d4769729
AdamZvara <zvaradam@gmail.com>
2024-10-20 03:56:57 -0400

    Fix histogram equalization

--- bvein/src/preprocess/histogram.py
+++ bvein/src/preprocess/histogram.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2
+from skimage.exposure import equalize_hist, rescale_intensity
 from .template import BVeinPreprocessor
 from typing import Tuple
 
@@ -11,6 +11,8 @@ class HistogramEqualization(BVeinPreprocessor):
     def preprocess(self, img: np.ndarray, mask: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """ Apply histogram equalization to the image.
 
+        This code was taken from https://github.com/bioidiap/bob.bio.vein/blob/master/src/bob/bio/vein/preprocessor/filters.py
+
         Args:
             img (np.ndarray): The image to be equalized.
             mask (np.ndarray): The mask to be equalized.
@@ -19,7 +21,10 @@ class HistogramEqualization(BVeinPreprocessor):
             tuple: A tuple containing the equalized image and mask.
         """
         # Apply histogram equalization to the image
-        equalized_img = cv2.equalizeHist(img)
+        retval = rescale_intensity(equalize_hist(img, mask=mask), out_range=(0, 255))
+
+        # Make the parts outside the mask totally black
+        retval[~mask] = 0
 
         # Return the equalized image and the original mask
-        return equalized_img, mask
+        return retval, mask
