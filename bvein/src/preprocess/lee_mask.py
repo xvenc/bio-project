e2e29f2ee4ec79c9ae0c2d2e21505f2d77c13195
AdamZvara <zvaradam@gmail.com>
2024-10-21 03:59:33 -0400

    Remove second modification to LeeMask

--- bvein/src/preprocess/lee_mask.py
+++ bvein/src/preprocess/lee_mask.py
@@ -99,19 +99,17 @@ class LeeMask(BVeinPreprocessor):
         return significant_differences
 
 class ModifiedLeeMask(LeeMask):
-    def __init__(self, filter_height=4, filter_width=40, mode='mode1', target_avg_height=0.0, batch_size=20, tolerance_percentage=0.15):
+    def __init__(self, filter_height=4, filter_width=40, target_avg_height=0.0, batch_size=20, tolerance_percentage=0.15):
         """
         Implement a modified version of the Lee mask method for finger vein recognition.
 
         :param filter_height: Height of the [1, -1] filter.
         :param filter_width: Width of the [1, -1] filter.
-        :param mode: The mode of operation for the modified Lee mask method.
         :param target_avg_height: The target average height for the columns.
         :param batch_size: The number of columns to process in each batch.
         :param tolerance_percentage: The tolerance percentage for the target average height.
         """
         super().__init__(filter_height, filter_width)
-        self.mode = mode
         self.target_avg_height = target_avg_height
         self.batch_size = batch_size
         self.tolerance_percentage = tolerance_percentage
@@ -120,13 +118,10 @@ class ModifiedLeeMask(LeeMask):
         original_lee_mask = self.preprocess(img, _mask)
         avg_height = self.get_avg_height(original_lee_mask[1])
 
-        if self.mode == 'mode1':
-            mask = self.adjust_columns_to_average_height(original_lee_mask[1], avg_height, self.batch_size)
-            mask = np.flipud(mask)
-            mask = self.adjust_columns_to_average_height(mask, avg_height, self.batch_size)
-            return (img, np.flipud(mask))
-        elif self.mode == 'mode2':
-            return (img, self.adjust_columns_to_average_height_2(original_lee_mask[1], self.target_avg_height, self.batch_size, self.tolerance_percentage))
+        mask = self.adjust_columns_to_average_height(original_lee_mask[1], avg_height, self.batch_size)
+        mask = np.flipud(mask)
+        mask = self.adjust_columns_to_average_height(mask, avg_height, self.batch_size)
+        return (img, np.flipud(mask))
 
     def get_avg_height(self, mask: np.ndarray):
         img_h, img_w = mask.shape
@@ -185,58 +180,5 @@ class ModifiedLeeMask(LeeMask):
                         # Set pixels to True to increase the height
                         for row in range(current_height, min(current_height + height_diff, half_img_h)):
                             mask[row, col] = False
-
-        return mask
-
-    def adjust_columns_to_average_height_2(self, mask: np.ndarray, target_avg_height: float, batch_size=20, tolerance_percentage=0.15) -> np.ndarray:
-        """
-        Adjust the columns of the mask to match the target average height with a tolerance percentage.
-
-        :param mask: np.ndarray of bools representing the mask.
-        :param target_avg_height: The target average height for the columns.
-        :param batch_size: The number of columns to process in each batch.
-        :param tolerance_percentage: The tolerance percentage for the target average height.
-        """
-        img_h, img_w = mask.shape
-        half_img_h = img_h // 2  # Middle point of the image (integer division)
-
-        # Arrays to store top border pixel positions in the upper half
-        top_pixels_upper_half = np.full(img_w, -1, dtype=int)
-
-        # Iterate over each column to find top-most `True` values in the upper half
-        for col in range(img_w):
-            # Get indices of `True` values in the upper half
-            true_indices_upper_half = np.where(mask[:half_img_h, col])[0]
-
-            if len(true_indices_upper_half) > 0:
-                top_pixels_upper_half[col] = true_indices_upper_half[0]  # Top-most `True` value in the upper half
-
-        # Define tolerance range around the target average height based on the tolerance percentage
-        tolerance = tolerance_percentage * target_avg_height
-        lower_bound = target_avg_height - tolerance
-        upper_bound = target_avg_height + tolerance
-
-        # Adjust columns based on average heights in the upper half
-        for start_col in range(0, img_w, batch_size):
-            end_col = min(start_col + batch_size, img_w)  # Ensure we don't go out of bounds
-            valid_heights = top_pixels_upper_half[start_col:end_col][top_pixels_upper_half[start_col:end_col] != -1]  # Get valid heights
-
-            if len(valid_heights) > 0:
-                avg_height = np.mean(valid_heights)  # Compute the average height
-                for col in range(start_col, end_col):
-                    current_height = top_pixels_upper_half[col]
-
-                    if current_height != -1:
-                        # Check if the current height is outside the tolerance range of the target_avg_height
-                        if current_height < lower_bound:
-                            # Adjust the height of this column upwards to the lower bound
-                            height_diff = lower_bound - current_height
-                            for row in range(int(current_height), int(min(current_height + height_diff, half_img_h))):
-                                mask[row, col] = False
-                        elif current_height > upper_bound:
-                            # Adjust the height of this column downwards to the upper bound
-                            height_diff = current_height - upper_bound
-                            for row in range(int(upper_bound), int(current_height)):
-                                mask[row, col] = True
 
         return mask
