16cd4e6d9d6f8e9a7c6fe3ed3e28d787505f006d
AdamZvara <zvaradam@gmail.com>
2024-10-27 13:46:08 -0400

    Redo gabor filters

--- bvein/gabor.py
+++ bvein/gabor.py
@@ -1,26 +1,27 @@
-from tqdm import tqdm
-
 from src.db import FingerVeinDatabase
 from src.preprocess_wrapper import PreprocessWrapper
 from src.extraction_wrapper import ExtractionWrapper
 
 from src.preprocess.lee_mask import ModifiedLeeMask
 from src.preprocess.cropper import MaskCropper, Cropper
+from src.preprocess.histogram import HistogramEqualization
 from src.extractors.gaborFilters import GaborFilters
 
-import matplotlib.pyplot as plt
+import random
 
+random.seed(10)
+
+preprocessing = [ModifiedLeeMask(), MaskCropper(), HistogramEqualization()]
+extractor = [GaborFilters(count=8)]
 
 db = FingerVeinDatabase()
-
-preprocessing = [ModifiedLeeMask(), MaskCropper(), Cropper(10, 10, 10, 10)]
-#preprocessing = [ModifiedLeeMask()]
 imgprep = PreprocessWrapper(preprocessing)
-
-extractor = [GaborFilters()]
 extractors = ExtractionWrapper(extractor)
 
-for i, batch in enumerate(db.get_random_batch_N(1, 1)):
+# Plot filters
+extractors.extractor_funcs[0].plot_filters()
+
+for i, batch in enumerate(db.get_random_batch_N(1, 5)):
     # Extract the veins from the images
     print(f"Batch {i}")
 
@@ -29,9 +30,6 @@ for i, batch in enumerate(db.get_random_batch_N(1, 1)):
     non_target = [imgprep.apply_preprocessing(img) for img in batch["non_target"]]
 
     # Extract the veins
-    target_ext = [extractors.extract(img_and_mask)[0] for (img_and_mask) in tqdm(target)]
-    non_target_ext = [extractors.extract(img_and_mask)[0] for (img_and_mask) in tqdm(non_target)]
-
-    #for img in target_ext + non_target_ext:
-    #    plt.imshow(img, cmap="gray")
-    #    plt.show()
+    for img_and_mask in target + non_target:
+        extractors.extract(img_and_mask)
+        extractors.extractor_funcs[0].show()
--- bvein/src/extractors/gaborFilters.py
+++ bvein/src/extractors/gaborFilters.py
@@ -1,14 +1,23 @@
 from .template import BVeinExtractor
 import cv2
 import numpy as np
+
 import matplotlib.pyplot as plt
-import math
-from skimage import morphology
-from skimage.util import invert
 
 class GaborFilters(BVeinExtractor):
-    def __init__(self, kernel_size=31, sigma=3.3, lambd=8, gamma=4.5, psi=0.89, ktype=cv2.CV_32F):
-    #def __init__(self, kernel_size=11, sigma=9, lambd=8, gamma=1/17, psi=0.89, ktype=cv2.CV_32F):
+    def __init__(self, kernel_size=31, sigma=4, lambd=9, gamma=0.5, psi=0, ktype=cv2.CV_32F, count=4):
+        """ Initialize the Gabor filter extractor with the given parameters for gabor filters
+
+        Args:
+            kernel_size (int): The size of the kernel
+            sigma (int): The standard deviation of the gaussian envelope
+            lambd (int): The wavelength of the sinusoidal factor
+            gamma (float): The spatial aspect ratio
+            psi (float): The phase offset
+            ktype (int): The type of the filter coefficients
+            count (int): The number of filters (and the number of orientations) to build
+
+        """
         self.kernel_size = kernel_size
         self.sigma = sigma
         self.lambd = lambd
@@ -16,44 +25,99 @@ class GaborFilters(BVeinExtractor):
         self.psi = psi
         self.ktype = ktype
 
-    def first_filter(self, image):
-        img_blur = cv2.blur(image, (5, 5))
-        return img_blur
-    
-    def rotation_correction(self, img, dst, middle_right, middle_left, w1, w2):
-        tangent_value = float(middle_right - middle_left) / float(w2 - w1)
-        rotation_angle = np.arctan(tangent_value)/math.pi*180
-        (h,w) = img.shape
-        center = (w // 2,h // 2)
-        M = cv2.getRotationMatrix2D(center,rotation_angle,1)#旋转缩放矩阵：(旋转中心，旋转角度，缩放因子)
-        rotated_dst = cv2.warpAffine(dst,M,(w,h))
-        rotated_img = cv2.warpAffine(img,M,(w,h))
-        return rotated_dst, rotated_img
+        # Build the filters
+        self.filters = self.build_filters(count)
 
-    def build_filters(self):
+    def build_filters(self, count : int) -> list:
+        """ Build a set of gabor filters rotated by theta in the range [0, pi) with a step of pi/count
+
+        Args:
+            count (int): The number of filters to build
+
+        Returns:
+            filters: A list of gabor filters
+        """
         filters = []
         ksize = self.kernel_size
-        for theta in np.arange(0, np.pi, np.pi / 4):
+        for theta in np.arange(0, np.pi, np.pi / count):
             kern = cv2.getGaborKernel((ksize, ksize), self.sigma, theta, self.lambd, self.gamma, self.psi, ktype=self.ktype)
             kern /= 1.5*kern.sum()
             filters.append(kern)
         return filters
 
-    def gabor_filter(self, img, filters):
+    def plot_filters(self) -> None:
+        # Plot the filters
+        fig, axes = plt.subplots(1, len(self.filters))
+        fig.suptitle("Filter Bank")
+        for i, kern in enumerate(self.filters):
+            axes[i].imshow(np.real(kern), cmap="gray")
+            axes[i].axis("off")
+        plt.tight_layout()
+        plt.show()
+
+    def gabor_filter(self, img: np.ndarray) -> np.ndarray:
+        """ Apply the gabor filter to the image """
+        # Store the filtered images and the accumulative image for later use
+        self.fimgs = []
+        self.accums = []
+
+        # Accumulative image of the best filter responses
         accum = np.zeros_like(img)
-        for kern in filters:
+        for kern in self.filters:
             fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
-            np.maximum(accum, fimg, accum)
+            accum = np.maximum(accum, fimg)
+            self.fimgs.append(fimg)
+            self.accums.append(accum)
+
         return accum
 
-    def extract(self, image_and_mask):
-        image, mask = image_and_mask
+    def extract(self, image_and_mask: tuple[np.ndarray, np.ndarray]) -> np.ndarray:
+        """ Extract the veins from the image using the gabor filters
 
-        img_blur = self.first_filter(image.copy())
+        Args:
+            image_and_mask: A tuple containing the image and the mask
 
-        enhanced_img = self.img_resized_enhance(img_blur)
+        Returns:
+            image: The extracted veins from the image
+        """
+        image, _ = image_and_mask
+        self._image_and_mask = image_and_mask
 
-        gab_filters = self.build_filters()
-        gabor_image = self.gabor_filter(enhanced_img, gab_filters)
+        # Invert the image to make the veins white (for canny edge detection)
+        image = np.apply_along_axis(lambda x: 255 - x, 0, image)
 
-        return gabor_image
+        # Apply gaussian blur
+        image = cv2.blur(image, (3, 3))
+
+        # Apply gabor filter
+        image = self.gabor_filter(image)
+        self._ext_veins_img = image
+
+        # Apply canny edge detection
+        image = cv2.Canny(image.astype(np.uint8), 90, 150)
+        self._ext_canny_img = image
+
+        return image
+
+    def show(self) -> None:
+        _, axes = plt.subplots(len(self.fimgs), 2, figsize=(15, 10))
+
+        # Show the filtered images and the accumulative images
+        for i, (img, accum) in enumerate(zip(self.fimgs, self.accums)):
+            axes[i][0].imshow(img, cmap="gray")
+            axes[i][1].imshow(accum, cmap="gray")
+
+        list(map(lambda x: x.axis("off"), axes.flatten()))
+        plt.tight_layout()
+        plt.show()
+
+        _, axes = plt.subplots(1, 3)
+
+        # Show the original image, the extracted veins, and the canny edge detection
+        axes[0].imshow(self._image_and_mask[0], cmap="gray")
+        axes[1].imshow(self._ext_veins_img, cmap="gray")
+        axes[2].imshow(self._ext_canny_img, cmap="gray")
+
+        list(map(lambda x: x.axis("off"), axes.flatten()))
+        plt.tight_layout()
+        plt.show()
